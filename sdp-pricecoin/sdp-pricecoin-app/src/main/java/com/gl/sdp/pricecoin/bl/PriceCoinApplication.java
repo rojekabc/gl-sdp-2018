d1df9a8f453d987818558c341fd9b48ba68bf06e
Piotr Rojewski <rojek_abc@o2.pl>
2018-05-08 23:04:24 +0200

    Hello world

--- sdp-pricecoin/sdp-pricecoin-app/src/main/java/com/gl/sdp/pricecoin/bl/PriceCoinApplication.java
+++ sdp-pricecoin/sdp-pricecoin-app/src/main/java/com/gl/sdp/pricecoin/bl/PriceCoinApplication.java
@@ -2,6 +2,7 @@ package com.gl.sdp.pricecoin.bl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PriceCoinApplication {
     public static void main(String[] args) {
         SpringApplication.run(PriceCoinApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String hello() {
+        return "<HTML><BODY><i>This is <b>SDP Price Coin</b> web application. Hello !!!</i></BODY></HTML>";
     }
 
 }
