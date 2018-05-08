d96449473277e0fbaa05b76b64e9a5603c710683
Piotr Rojewski <rojek_abc@o2.pl>
2018-05-08 23:27:41 +0200

    Autoscan

--- sdp-pricecoin/sdp-pricecoin-app/src/main/java/com/gl/sdp/pricecoin/PriceCoinApplication.java
+++ sdp-pricecoin/sdp-pricecoin-app/src/main/java/com/gl/sdp/pricecoin/PriceCoinApplication.java
@@ -1,4 +1,4 @@
-package com.gl.sdp.pricecoin.bl;
+package com.gl.sdp.pricecoin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
